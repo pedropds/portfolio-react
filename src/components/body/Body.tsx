cb9f339dd0ca9f72321378cdb1ce3d13dfbbc2bf
Pedro Sousa <iampintuu6@gmail.com>
2024-12-09 17:45:50 +0000

    added description small css changes

--- src/components/body/Body.tsx
+++ src/components/body/Body.tsx
@@ -11,14 +11,22 @@ export const Body = ({ isDarkTheme }: BodyProps) => {
   return (
     <div className={`body ${isDarkTheme ? "dark-theme" : "light-theme"}`}>
       <header
-        className={`text-element ${isDarkTheme ? "dark-theme" : "light-theme"}`}
+        className={`text-element header-text ${
+          isDarkTheme ? "dark-theme" : "light-theme"
+        }`}
       >
         Pedro Sousa
       </header>
       <span
-        className={`text-element ${isDarkTheme ? "dark-theme" : "light-theme"}`}
+        className={`text-element description-text ${
+          isDarkTheme ? "dark-theme" : "light-theme"
+        }`}
       >
-        Description about me
+        I am Pedro Sousa, a Software Engineer with a passion for continuous
+        learning and innovation. Currently working at Microsoft, I contribute as
+        part of the Feedback team for Office 365, specializing in developing and
+        enhancing full-stack solutions using technologies like C++, TypeScript,
+        JavaScript, and React.
       </span>
       <MyTimeline items={timelineData} isDarkTheme={isDarkTheme} />;
     </div>
